990e76566b3128c99b4708c1114d8d8ae855d548
nodax-hub <ioann.ishenko.2003@gmail.com>
2023-04-14 16:46:42 +0400

    Update index.py

--- alice/index.py
+++ alice/index.py
@@ -1,6 +1,6 @@
-import datetime
 from parser import today, tomorrow
 from schedule import Day
+
 
 def say_day(day: Day) -> str:
     response = f"Расписание на {day.date.strftime('%d.%m.%Y')}.\nИ так слушайте:\n"
@@ -18,17 +18,17 @@ def handler(event: dict, context) -> dict:
     phrase = event['request']['original_utterance'].lower()
 
     if event['session']['new'] and len(phrase) == 0:
-        text = 'Привет. Для того чтобы узнать своё расписание просто спроси меня: "расписание на сегодня".' +
+        text = 'Привет. Для того чтобы узнать своё расписание просто спроси меня: "расписание на сегодня". ' \
                'Если возникнут вопросы скажите помощь.'
 
     elif 'помощь' in phrase:
-        text = 'Для того чтобы я могла сказать ваше расписание, мне необходимо получить от вас id группы.' +
-               'Затем вы можете попросить меня сказать рассписание на сегодня или на завтра.'
+        text = 'Для того чтобы я могла сказать ваше расписание, мне необходимо получить от вас id группы. ' \
+               'Затем вы можете попросить меня сказать расписание на сегодня или на завтра.'
 
     elif 'спасибо' in phrase:
         text = 'Всегда рада помочь.'
         end_session = 'true'
-    
+
     elif phrase.isdigit():
         user_state_update['group_id'] = int(phrase)
         text = 'Вы успешно сменили id своей группы.'
@@ -38,7 +38,7 @@ def handler(event: dict, context) -> dict:
 
     elif 'сегодня' in phrase:
         text = say_day(today(event['state']['user']['group_id']))
-    
+
     elif 'завтра' in phrase:
         text = say_day(tomorrow(event['state']['user']['group_id']))
 
